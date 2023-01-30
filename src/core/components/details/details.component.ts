03033ca22d616cfd81bc9ee9fa22f9d3962625de
Akash <89532176+Ethan341@users.noreply.github.com>
2023-01-31 00:30:52 +0530

    Sorted movies according to genere and added state movieDetails

--- src/core/components/details/details.component.ts
+++ src/core/components/details/details.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-details',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetailsComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private ActivatedRoute:ActivatedRoute,
+    private Router : Router) {
+      let movieDetails = this.Router.getCurrentNavigation()?.extras.state
+      console.log(movieDetails);
+   }
 
   ngOnInit(): void {
+    
   }
 
 }
--- src/core/components/movies-dashboard/movies-dashboard.component.ts
+++ src/core/components/movies-dashboard/movies-dashboard.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-movies-dashboard',
@@ -7,9 +9,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MoviesDashboardComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private http : HttpClient,
+    private Router: Router) { }
+  moviesCollection: any= []
   ngOnInit(): void {
+    this.fetchMovies()
+    
   }
 
+  fetchMovies(){
+    this.http.get('https://api.tvmaze.com/shows').subscribe((Response)=>{
+      console.log(Response);
+      this.moviesCollection = Response
+      this.filterAllGenere(Response)
+    })
+  }
+
+  filterAllGenere(Response:any){
+    let genere : any = []
+    Response.forEach((movie: any) => {
+      let exist = false; 
+      for(let i = 0 ; i< genere.length; i++){
+          if(genere[i].genereName == movie.genres[0]){
+            exist = true;
+            genere[i].speceficGenereCollection.push(movie);
+          }
+      }
+      if(!exist){
+        genere.push({genereName: movie.genres[0], speceficGenereCollection:[movie] })
+      }
+    });
+    console.log(genere);
+    this.moviesCollection = genere
+  }
+
+  showMovieDetails(movie:any){
+    console.log("Routing to details");
+    this.Router.navigate(['details'],{state: movie});
+  }
 }
