1c91d39b17cf4e0ce3bf4e28486b23c99ca392e4
SHUBHAM  WAGH <waghshubham2712@gmail.com>
2023-01-30 22:17:39 +0530

    start 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './component/home/home.component';
+import { ListViewComponent } from './component/list-view/list-view.component';
+import { ListViewModule } from './component/list-view/list-view.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ListViewComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ListViewModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/component/list-view/list-view.module.ts
@@ -0,0 +1,5 @@
+import { NgModule } from "@angular/core";
+import { ListViewComponent } from "./list-view.component";
+
+@NgModule({ declarations: [] })
+export class ListViewModule { }
